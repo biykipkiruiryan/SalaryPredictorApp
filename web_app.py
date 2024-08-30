d0ccfab0d9b82a32ce7d08e64add406d8e03d5ae
Your Name <your.email@example.com>
2024-08-30 11:24:58 +0300

    added creators files

--- web_app.py
+++ web_app.py
@@ -2,11 +2,7 @@ import pickle
 import numpy as np
 import streamlit as st
 
-model_path = os.path.join(os.path.dirname(__file__), 'model.pkl')
-if not os.path.isfile(model_path):
-    raise FileNotFoundError(f"Model file {model_path} not found.")
-model = pickle.load(open(model_path, 'rb'))
-
+model = pickle.load(open('model.pkl', 'rb'))
 
 col0, col1, col2, col3, col4, col5, col6 = st.columns(7)
 with col0:
@@ -70,4 +66,5 @@ if(predict_btn):
         st.text(f"Estimated salary: ${int(salary[0])}")
     with col17:
         st.write('')
+
 
